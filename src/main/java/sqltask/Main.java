747e4bd0c56c77083508d59a942ea944710f9f9e
xanthine <xanthine2002@gmail.com>
2022-06-21 13:42:39 +0400

    changes in sql script

--- src/main/java/sqltask/Main.java
+++ src/main/java/sqltask/Main.java
@@ -2,7 +2,8 @@ package sqltask;
 
 public class Main {
 
+    @SuppressWarnings("java:S106")
     public static void main(String[] args) {
-
+        System.out.println("abc");
     }
 }
--- src/main/resources/tables_creation.sql
+++ src/main/resources/tables_creation.sql
@@ -1,17 +1,14 @@
 
 DROP TABLE IF EXISTS public."GROUPS";
-CREATE TABLE IF NOT EXISTS public."GROUPS"
+CREATE TABLE public."GROUPS"
 (
     group_id integer NOT NULL,
     group_name character(6) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "GROUPS_pkey" PRIMARY KEY (group_id)
 );
 
-ALTER TABLE IF EXISTS public."GROUPS"
-    OWNER to postgres;
-
 DROP TABLE IF EXISTS public."STUDENTS";
-CREATE TABLE IF NOT EXISTS public."STUDENTS"
+CREATE TABLE public."STUDENTS"
 (
     student_id integer NOT NULL,
     group_id integer,
@@ -20,17 +17,11 @@ CREATE TABLE IF NOT EXISTS public."STUDENTS"
     CONSTRAINT "STUDENTS_pkey" PRIMARY KEY (student_id)
 );
 
-ALTER TABLE IF EXISTS public."STUDENTS"
-    OWNER to postgres;
-
 DROP TABLE IF EXISTS public."COURSES";
-CREATE TABLE IF NOT EXISTS public."COURSES"
+CREATE TABLE public."COURSES"
 (
     course_id integer NOT NULL,
     course_name character(255) COLLATE pg_catalog."default" NOT NULL,
     course_description character(510) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT "COURSES_pkey" PRIMARY KEY (course_id)
 );
-
-ALTER TABLE IF EXISTS public."COURSES"
-    OWNER to postgres;
